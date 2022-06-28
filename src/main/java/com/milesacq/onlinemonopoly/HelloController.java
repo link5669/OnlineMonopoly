2de59e50deee2153302b77cc5fac0d1547c6e4f4
link5669 <milesacq@icloud.com>
2022-06-27 21:32:11 -0400

    Added FXML tags for each piece

--- src/main/java/com/milesacq/onlinemonopoly/HelloController.java
+++ src/main/java/com/milesacq/onlinemonopoly/HelloController.java
@@ -15,6 +15,83 @@ public class HelloController {
     @FXML
     private Rectangle one;
     @FXML
+    private Rectangle two;
+    @FXML
+    private Rectangle three;
+    @FXML
+    private Rectangle four;
+    @FXML
+    private Rectangle five;
+    @FXML
+    private Rectangle six;
+    @FXML
+    private Rectangle seven;
+    @FXML
+    private Rectangle eight;
+    @FXML
+    private Rectangle nine;
+    @FXML
+    private Rectangle ten;
+    @FXML
+    private Rectangle eleven;
+    @FXML
+    private Rectangle twelve;
+    @FXML
+    private Rectangle thirteen;
+    @FXML
+    private Rectangle fourteen;
+    @FXML
+    private Rectangle fifteen;
+    @FXML
+    private Rectangle sixteen;
+    @FXML
+    private Rectangle seventeen;
+    @FXML
+    private Rectangle eighteen;
+    @FXML
+    private Rectangle nineteen;
+    @FXML
+    private Rectangle twenty;
+    @FXML
+    private Rectangle twentyOne;
+    @FXML
+    private Rectangle twentyTwo;
+    @FXML
+    private Rectangle twentyThree;
+    @FXML
+    private Rectangle twentyFour;
+    @FXML
+    private Rectangle twentyFive;
+    @FXML
+    private Rectangle twentySix;
+    @FXML
+    private Rectangle twentySeven;
+    @FXML
+    private Rectangle twentyEight;
+    @FXML
+    private Rectangle twentyNine;
+    @FXML
+    private Rectangle thirty;
+    @FXML
+    private Rectangle thirtyOne;
+    @FXML
+    private Rectangle thirtyTwo;
+    @FXML
+    private Rectangle thirtyThree;
+    @FXML
+    private Rectangle thirtyFour;
+    @FXML
+    private Rectangle thirtyFive;
+    @FXML
+    private Rectangle thirtySix;
+    @FXML
+    private Rectangle thirtySeven;
+    @FXML
+    private Rectangle thirtyEight;
+    @FXML
+    private Rectangle thirtyNine;
+
+    @FXML
     private Circle pOne;
     @FXML
     private VBox boardBox;
